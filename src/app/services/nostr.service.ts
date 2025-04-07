844f2c6b10d4e83f27d3cd0f64b9a29a789dcc7d
SondreB <sondre@outlook.com>
2025-04-07 02:57:26 +0200

    feat: add mobile navigation visibility control for menu items

--- src/app/services/nostr.service.ts
+++ src/app/services/nostr.service.ts
@@ -130,6 +130,7 @@ export class NostrService {
   }
 
   usePreviewAccount(): void {
+    // jack
     const previewPubkey = '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2';
     this.user.set({
       pubkey: previewPubkey,
