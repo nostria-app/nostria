ef57e178e1af0fbbe16031d6584eefee0df96235
SondreB <sondre@outlook.com>
2025-05-11 13:50:10 +0200

    Add a beta indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,8 +59,7 @@ interface NavItem {
     MatFormFieldModule,
     NPubPipe,
     MatBadgeModule
-  ],
-  templateUrl: './app.component.html',
+  ],  templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
