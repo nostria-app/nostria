85397fa541c24ccdd0d56ff87c659a8254982f1f
SondreB <sondre@outlook.com>
2025-05-15 16:58:40 +0200

    feat: Implement account management features with account switching and removal

--- src/app/pages/accounts/accounts.component.ts
+++ src/app/pages/accounts/accounts.component.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ConfirmDialogComponent, ConfirmDialogData } from '../../components/confirm-dialog/confirm-dialog.component';
+import { NostrService } from '../../services/nostr.service';
+import { LoggerService } from '../../services/logger.service';
+import { NPubPipe } from '../../pipes/npub.pipe';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  standalone: true,  imports: [
+    CommonModule,
+    RouterLink,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatTooltipModule,
+    NPubPipe
+  ],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.scss'
 })
 export class AccountsComponent {
+  nostrService = inject(NostrService);
+  private dialog = inject(MatDialog);
+  private logger = inject(LoggerService);
+  
+  currentAccount = this.nostrService.account;
 
+  /**
+   * Remove an account after confirmation
+   * @param event The click event
+   * @param pubkey The public key of the account to remove
+   */
+  removeAccount(event: Event, pubkey: string): void {
+    // Prevent click event from propagating
+    event.stopPropagation();
+    this.logger.debug('Attempting to remove account', { pubkey });
+    
+    // Show confirmation dialog
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'Remove Account',
+        message: 'If you do not have backup of your nsec for this account, your account will be permanetly deleted and lost. Only if you have a backup, will you be able to restore it again. Are you sure?',
+        confirmText: 'Remove Account',
+        cancelText: 'Cancel',
+        confirmColor: 'warn'
+      } as ConfirmDialogData
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.logger.debug('Removing account confirmed', { pubkey });
+        this.nostrService.removeAccount(pubkey);
+      }
+    });
+  }
+
+  /**
+   * Get metadata for an account
+   * @param pubkey The public key of the account
+   */
+  getMetadataForAccount(pubkey: string) {
+    const metadata = this.nostrService.getMetadataForAccount(pubkey);
+    return metadata?.content;
+  }
+
+  /**
+   * Switch to a different account
+   * @param pubkey The public key to switch to
+   */
+  switchAccount(pubkey: string): void {
+    this.logger.debug('Switching to account', { pubkey });
+    this.nostrService.switchToUser(pubkey);
+  }
 }
