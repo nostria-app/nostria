6e2ce00860fe9b0a40cff1314deb7fdc1b03cd5e
SondreB <sondre@outlook.com>
2025-05-31 09:16:23 +0200

    feat: Remove welcome component and associated routes, styles, and template for a cleaner codebase

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,18 +1,18 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'p/**',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'e/**',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'about/**',
-    renderMode: RenderMode.Server
-  },
+  // {
+  //   path: 'p/**',
+  //   renderMode: RenderMode.Client
+  // },
+  // {
+  //   path: 'e/**',
+  //   renderMode: RenderMode.Server
+  // },
+  // {
+  //   path: 'about/**',
+  //   renderMode: RenderMode.Server
+  // },
   {
     path: '**',
     renderMode: RenderMode.Client
