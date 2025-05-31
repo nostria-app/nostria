8295eef2de2cfc5010ecdefc451004f57a43edca
SondreB <sondre@outlook.com>
2025-05-31 11:29:04 +0200

    feat: Add NoteEditorDialog component with template, styles, and tests

---
+++ src/app/components/note-editor-dialog/note-editor-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-editor-dialog',
+  imports: [],
+  templateUrl: './note-editor-dialog.component.html',
+  styleUrl: './note-editor-dialog.component.scss'
+})
+export class NoteEditorDialogComponent {
+
+}
