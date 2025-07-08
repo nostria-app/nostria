65a71961da89465c2fb328d59fd2b374dd4536f7
SondreB <sondre@outlook.com>
2025-07-08 08:55:27 +0200

    feat: Implement messages routing and structure with main and list components. Remove box-sizing from CSS, onle single global assignment.

---
+++ src/app/pages/messages/list/list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-list',
+  imports: [],
+  templateUrl: './list.html',
+  styleUrl: './list.scss'
+})
+export class MessagesList {
+
+}
---
+++ src/app/pages/messages/main/main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-main',
+  imports: [],
+  templateUrl: './main.html',
+  styleUrl: './main.scss'
+})
+export class MessagesMain {
+
+}
