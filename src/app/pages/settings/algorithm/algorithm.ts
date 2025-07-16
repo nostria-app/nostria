6312dc81ccd70d053d79cf0f6a32c078bb14536c
SondreB <sondre@outlook.com>
2025-07-16 11:32:44 +0200

    feat(settings): add Algorithm settings section and component

---
+++ src/app/pages/settings/algorithm/algorithm.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-algorithm',
+  imports: [],
+  templateUrl: './algorithm.html',
+  styleUrl: './algorithm.scss'
+})
+export class AlgorithmComponent {
+
+}
