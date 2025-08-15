4c29cb0cf205dbc657d657397a896b90129690ec
Kosta Korenkov <kosta@korenkov.net>
2025-08-15 17:34:20 +0700

    feat: support Delete/Retraction Requests (NIP-09)

--- src/app/components/event/header/header.component.ts
+++ src/app/components/event/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component, effect, inject, input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -17,6 +24,13 @@ import { AgoPipe } from '../../../pipes/ago.pipe';
 import { DatePipe } from '@angular/common';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { MatCardModule } from '@angular/material/card';
+import {
+  ConfirmDialogComponent,
+  ConfirmDialogData,
+} from '../../confirm-dialog/confirm-dialog.component';
+import { NostrService } from '../../../services/nostr.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AccountStateService } from '../../../services/account-state.service';
 
 @Component({
   selector: 'app-event-header',
@@ -37,12 +51,24 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class EventHeaderComponent {
   readonly layout = inject(LayoutService);
+  accountState = inject(AccountStateService);
   dialog = inject(MatDialog);
   data = inject(DataService);
+  nostrService = inject(NostrService);
+  snackBar = inject(MatSnackBar);
 
   event = input.required<Event>();
 
   record = signal<NostrRecord | null>(null);
+
+  isOurEvent = computed<boolean>(() => {
+    const event = this.event();
+    if (!event) {
+      return false;
+    }
+
+    return event.pubkey === this.accountState.pubkey();
+  });
 
   constructor() {
     effect(() => {
@@ -54,6 +80,36 @@ export class EventHeaderComponent {
 
       const record = this.data.toRecord(event);
       this.record.set(record);
+    });
+  }
+
+  async deleteEvent() {
+    const event = this.event();
+    if (!event) {
+      return;
+    }
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'Delete event',
+        message: 'Are you sure?',
+        confirmText: 'Delete event',
+        cancelText: 'Cancel',
+        confirmColor: 'warn',
+      } as ConfirmDialogData,
+    });
+
+    dialogRef.afterClosed().subscribe(async result => {
+      if (result) {
+        const deleteEvent = this.nostrService.createRetractionEvent(event);
+
+        const published = await this.nostrService.signAndPublish(deleteEvent);
+        if (published) {
+          this.snackBar.open('Note deletion was requested', 'Dismiss', {
+            duration: 3000,
+          });
+        }
+      }
     });
   }
 
