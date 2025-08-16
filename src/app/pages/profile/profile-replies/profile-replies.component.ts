265f19e5bfddb46a8ec63d2afed8082b7f49e9f7
Kosta Korenkov <kosta@korenkov.net>
2025-08-16 11:08:00 +0700

    fix: Profile Replies to show data (#121)
    Fixed by using the Event Component.

--- src/app/pages/profile/profile-replies/profile-replies.component.ts
+++ src/app/pages/profile/profile-replies/profile-replies.component.ts
@@ -1,24 +1,24 @@
-import { Component, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { NostrService } from '../../../services/nostr.service';
-import { LoggerService } from '../../../services/logger.service';
-import { ProfileStateService } from '../../../services/profile-state.service';
-import { MatCardModule } from '@angular/material/card';
-import { UserProfileComponent } from '../../../components/user-profile/user-profile.component';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { BookmarkService } from '../../../services/bookmark.service';
-import { AgoPipe } from '../../../pipes/ago.pipe';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ContentComponent } from '../../../components/content/content.component';
+import { EventComponent } from '../../../components/event/event.component';
+import { UserProfileComponent } from '../../../components/user-profile/user-profile.component';
+import { AgoPipe } from '../../../pipes/ago.pipe';
+import { BookmarkService } from '../../../services/bookmark.service';
+import { ProfileStateService } from '../../../services/profile-state.service';
 
 @Component({
   selector: 'app-profile-replies',
   standalone: true,
   imports: [
+    EventComponent,
     CommonModule,
     MatIconModule,
     MatCardModule,
@@ -37,8 +37,6 @@ import { ContentComponent } from '../../../components/content/content.component'
 })
 export class ProfileRepliesComponent {
   private route = inject(ActivatedRoute);
-  private nostrService = inject(NostrService);
-  private logger = inject(LoggerService);
   profileState = inject(ProfileStateService);
   bookmark = inject(BookmarkService);
   error = signal<string | null>(null);
