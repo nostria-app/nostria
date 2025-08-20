45ddcd600d9761b9900c505bf5220ca447c3cb2a
SondreB <sondre@outlook.com>
2025-08-21 00:20:20 +0200

    feat: Add support for music playlists and update column types in feed service

--- src/app/services/relays/discovery-relay.ts
+++ src/app/services/relays/discovery-relay.ts
@@ -11,8 +11,7 @@ import { kinds, SimplePool } from 'nostr-tools';
 })
 export class DiscoveryRelayServiceEx
   extends RelayServiceBase
-  implements NostriaService
-{
+  implements NostriaService {
   private readonly utilities = inject(UtilitiesService);
   private localStorage = inject(LocalStorageService);
   private appState = inject(ApplicationStateService);
@@ -58,7 +57,7 @@ export class DiscoveryRelayServiceEx
     this.initialized = true;
   }
 
-  clear() {}
+  clear() { }
 
   save(relayUrls: string[]) {
     // Save to local storage
